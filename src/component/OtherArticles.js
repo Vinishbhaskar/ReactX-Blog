ebd7c4758b0891bc7be2c603b53ab2fc851d21e9
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-02-22 14:41:12 +0530

    Added a useUser custom hook

--- src/component/OtherArticles.js
+++ src/component/OtherArticles.js
@@ -1,22 +1,22 @@
-import { useParams } from "react-router-dom"
-import articles from "../Pages/article-content"
+import { Link, useParams } from "react-router-dom";
+import articles from "../Pages/article-content";
 
 const OtherArticles = () => {
-  const {articleId} = useParams();
-  const otherArticles = articles.filter(article=> article.name !== articleId).slice(0,3);
+  const { articleId } = useParams();
+  const otherArticles = articles.filter(article => article.name !== articleId).slice(0, 3);
 
   return (
     <>
-    <h3>Other Articles:</h3>
+      <h3>Other Articles:</h3>
       <ul>
         {otherArticles.map((article, i) => (
           <li key={i}>
-            <a href={`/article/${article.name}`}>{article.title}</a>
+            <Link to={`/article/${article.name}`}>{article.title}</Link>
           </li>
         ))}
-      </ul> 
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default OtherArticles
+export default OtherArticles;
