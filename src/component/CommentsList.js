9f8f9d42d07b0cf3ed64c68987acc59c78e4e785
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-03-04 13:28:53 +0530

    Comment Maping

--- src/component/CommentsList.js
+++ src/component/CommentsList.js
@@ -1,15 +1,13 @@
-const CommentsList = ({comments}) => {
-  return (
-    <>
+const CommentsList = ({ comments }) => (
+  <>
     <h3>Comments:</h3>
-    {comments.map((comment, index) => (
-        <div className="comment" key={index}>
-            <h4>{comment.postedBy}</h4>
-            <p>{comment.text}</p>
-        </div>
+    {comments.map(comment => (
+      <div className="comment" key={`${comment.postedBy}:${comment.text}`}>
+        <h4>{comment.postedBy}</h4>
+        <p>{comment.text}</p>
+      </div>
     ))}
     </>
-  )
-}
+);
 
-export default CommentsList
+export default CommentsList;
